aad98d5d5126b8865040d411d0d1746cf3389858
Miloslav Kamenik <miloslav.kamenik@outlook.cz>
2023-02-28 15:42:54 +0100

    add new two py scripts

---
+++ full_name.py
@@ -0,0 +1,37 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+message = f"Hello, {full_name.title()}!"
+
+print(full_name)
+print(message)
+
+# Python piramid
+print("\t\tPython")
+print("\tPython", "\t\tPython")
+print("Python", "\t\t\t\tPython")
+
+#python renault
+print("\n\t\tPython")
+print("\tPython", "\t\tPython")
+print("Python", "\t\t\t\tPython")
+print("\tPython", "\t\tPython")
+print("\t\tPython")
+
+#studying
+
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#function to remove extra whitespace from the right
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
+
+#function to remove extra whitespace from the left
+favorite_language = ' python '
+print(favorite_language.lstrip())
+
+#function to remove extra whitespace from the both sides
+print(favorite_language.strip())
