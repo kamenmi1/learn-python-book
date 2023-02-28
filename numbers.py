b7952753136731e37172e41a5b1cc560ad51a4a6
Miloslav Kamenik <miloslav.kamenik@outlook.cz>
2023-02-28 16:16:31 +0100

    adding new script for numbers

---
+++ numbers.py
@@ -0,0 +1,27 @@
+#playing with numbers in python 
+
+print(3+5)
+print(3-5)
+print(5-2)
+print(5*3)
+print(5**3)
+print(5/3)
+
+#floats
+
+print(2.5+1)
+print(5.0/2)
+print(5+1.0)
+
+#easy way how to see amount of zeros - output without _
+universe_age = 14_000_000_000
+print(universe_age)
+
+#mutiple assignment of values 
+
+x,y,z = 0,0,0
+print(f"\n{x,y,z}\n{x}\n{y}\n{z}")
+
+#Constants - capital letters - other than that normal variable
+MAX_CONNECTIONS = 5000 
+
