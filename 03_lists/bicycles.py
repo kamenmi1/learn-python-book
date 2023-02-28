09fd8a078eea8810add7deacbbbf50cfc9241c7e
Miloslav Kamenik <miloslav.kamenik@outlook.cz>
2023-02-28 16:41:11 +0100

    adding folders for better visibility

---
+++ 03_lists/bicycles.py
@@ -0,0 +1,12 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#use index to print one of the items in the list 
+print(bicycles[0])
+print(bicycles[0].title())
+
+# use index -1 to return the last element
+print(bicycles[-1])
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
