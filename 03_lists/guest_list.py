ced808be2f03da0f1cd19dd227adbe86ea3f0cf6
Miloslav Kamenik <miloslav.kamenik@outlook.cz>
2023-03-04 11:05:08 +0100

    more py scripts for lists

---
+++ 03_lists/guest_list.py
@@ -0,0 +1,6 @@
+invitation = ['Aanchal', 'Lenicka', 'Dad', 'Mom']
+
+print(f'Hello, {invitation[0]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[1]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[2]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[3]}, I would like to invite you to dinner.')
---
+++ 03_lists/more_guests.py
@@ -0,0 +1,15 @@
+invitation = ['Aanchal', 'Lenicka', 'Dad', 'Mom']
+
+invitation.append('Steve')
+invitation.insert(0,'Vaclav')
+invitation.insert(3,'Benik')
+
+print(f'Hello, {invitation[0]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[1]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[2]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[3]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[4]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[5]}, I would like to invite you to dinner.')
+print(f'Hello, {invitation[6]}, I would like to invite you to dinner.\n\n')
+
+print(invitation)
